42881b8c4fe64551b00b01e22de3a91d47f6d0c7
GonzaOrtiz <gonzaortiz1506@gmail.com>
2021-11-13 11:33:51 -0300

    Agregar archivos de proyecto.

---
+++ _cubits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _cubits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
